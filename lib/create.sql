2f86a05cafa060b185554f5a9f5cacae2777931e
Oscar Cabrera <cabreraoscar0521@gmail.com>
2024-08-16 10:39:40 -0500

    tests completed

---
+++ lib/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER, 
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
+
+
+-- SELECT * FROM bears;
+
+-- DROP TABLE IF EXISTS bears;
